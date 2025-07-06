a64d7671ea82c69e7b0c3bb94645d0eb3d833015
Robherto Matthew <Robhertomatt@gmail.com>
2025-07-06 18:23:25 +0700

    Refactor platform components to split native and web
    Replaces the unified PlatformComponents.ts with separate PlatformComponents.native.ts and PlatformComponents.web.ts files. This change improves maintainability by isolating platform-specific imports and polyfills for native and web environments.

---
+++ src/utils/PlatformComponents.native.ts
@@ -0,0 +1,9 @@
+// src/utils/PlatformComponents.native.ts
+export { default as LinearGradient } from 'react-native-linear-gradient';
+export { default as MapView } from 'react-native-maps';
+export { default as DeviceInfo } from 'react-native-device-info';
+export { default as Geolocation } from 'react-native-geolocation-service';
+export * as ImagePicker from 'react-native-image-picker';
+export { default as ImageCropPicker } from 'react-native-image-crop-picker';
+export * as Keychain from 'react-native-keychain';
+export { default as SplashScreen } from 'react-native-splash-screen';
---
+++ src/utils/PlatformComponents.web.ts
@@ -0,0 +1,9 @@
+// src/utils/PlatformComponents.web.ts
+export { default as LinearGradient } from './webPolyfills/LinearGradientPolyfill';
+export { default as MapView } from './webPolyfills/MapViewPolyfill';
+export { default as DeviceInfo } from './webPolyfills/DeviceInfoPolyfill';
+export { default as Geolocation } from './webPolyfills/GeolocationPolyfill';
+export { default as ImagePicker } from './webPolyfills/ImagePickerPolyfill';
+export { default as ImageCropPicker } from './webPolyfills/ImagePickerPolyfill';
+export { default as Keychain } from './webPolyfills/KeychainPolyfill';
+export { default as SplashScreen } from './webPolyfills/SplashScreenPolyfill';
